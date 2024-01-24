f8b1f9a58ea17356192760ffb7aa5f65fe5dafa1
Rayanne <mouraribeiro11@gmail.com>
2024-01-23 22:41:01 -0300

    Criando componentes básicos

---
+++ src/ListComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ItemComponent from './ItemComponent';
+
+export default function ListComponent(){
+    return (
+        <div>
+             <h2> Minha Lista </h2>
+            <ul>
+                <ItemComponent name={'Meu item'} />
+                <ItemComponent name={'Meu item 2'} />
+            </ul>
+        </div>
+    )
+}
