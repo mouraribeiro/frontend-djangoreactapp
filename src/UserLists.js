a90906bc741f692d093fce197bf92e5402a4f696
Rayanne <mouraribeiro11@gmail.com>
2024-01-25 21:56:35 -0300

    "add map para buscar dados"

--- src/UserLists.js
+++ src/UserLists.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ListComponent from './ListComponent';
 
-export default function UserLists(){
-    return (
-        <div>
-            <ListComponent listName={'Minha Lista'} />
-            <ListComponent listName={'Minha Lista 2'} />
-        </div>
-    )
+export default class UserLists extends React.Component{
+    state = {lists: [], loading: true}
+
+    async componentDidMount(){
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+        config.headers['Authorization'] = 'Token befc0a5ac3c44b7885ed194de634d1613277606f';
+        var url = 'http://127.0.0.1:8000/list/';
+        const response = await fetch(url, config)
+        const data = await response.json();
+        this.setState({lists:data, loading:false})
+    }
+    render(){
+        const listApi= this.state.lists;
+        return (
+            <div>
+                {/* <ListComponent listName={'Minha Lista'} />
+                <ListComponent listName={'Minha Lista 2'} /> */}
+                {listApi.map(list => <ListComponent key={list.id} listName={list.name}/>)}
+            </div>
+        )
+    }
+    
 }
